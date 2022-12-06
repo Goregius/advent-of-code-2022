57e2ff082f5b5ee88971610343a8b9ae46247a7a
George Wilkins <georgewilkins99@gmail.com>
2022-12-06 23:19:24 +0000

    feat(day-4): add day 4 solution

---
+++ src/main/kotlin/day4/Main.kt
@@ -0,0 +1,32 @@
+package day4
+
+import utils.readInput
+
+private fun main() {
+    val input = readInput(4)
+
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: String): Int = input.lines().count { line ->
+    val (firstSections, secondSections) = parseLine(line)
+
+    firstSections.first in secondSections &&
+            firstSections.last in secondSections ||
+            secondSections.first in firstSections &&
+            secondSections.last in firstSections
+}
+
+private fun part2(input: String): Int = input.lines().count { line ->
+    val (firstSections, secondSections) = parseLine(line)
+
+    firstSections.any { it in secondSections }
+}
+
+private fun parseLine(line: String) = line
+    .split(",")
+    .map { rangeText ->
+        val rangeList = rangeText.split("-").map { it.toInt() }
+        (rangeList[0]..rangeList[1])
+    }
