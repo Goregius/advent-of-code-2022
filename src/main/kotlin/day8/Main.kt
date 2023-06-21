5ace10ed6249d36f500dcf7a2cb488c69feb98af
George Wilkins <georgewilkins99@gmail.com>
2023-06-21 03:09:30 +0100

    refactor(day-8): combine logic

--- src/main/kotlin/day8/Main.kt
+++ src/main/kotlin/day8/Main.kt
@@ -10,55 +10,19 @@ private fun main() {
     println(part2(treeGrid))
 }
 
-private fun part1(treeGrid: List<List<Int>>): Int {
-    return treeGrid.flatMapIndexed { rowIndex, row ->
-        List(row.size) { colIndex ->
-            isTreeVisibleInGrid(treeGrid = treeGrid, rowIndex = rowIndex, colIndex = colIndex)
-        }
-    }.count { it }
-}
+private fun part1(treeGrid: List<List<Int>>): Int =
+    treesWithTreesInAllDirections(treeGrid).count { (tree, treesInAllDirections) ->
+        treesInAllDirections
+            .map { trees -> trees.all { it < tree } }
+            .any { it }
+    }
 
-private fun part2(treeGrid: List<List<Int>>): Int {
-    return treeGrid.flatMapIndexed { rowIndex, row ->
-        List(row.size) { colIndex ->
-            calculateScenicScore(treeGrid = treeGrid, rowIndex = rowIndex, colIndex = colIndex)
-        }
-    }.max()
-}
-
-private fun isTreeVisibleInGrid(treeGrid: List<List<Int>>, rowIndex: Int, colIndex: Int): Boolean {
-    if (isTreeAnEdge(treeGrid, rowIndex, colIndex)) return true
-
-    val row = treeGrid[rowIndex]
-    val col = treeGrid.indices.map { treeGrid[it][colIndex] }
-
-    return isTreeVisibleInLine(row, colIndex) || isTreeVisibleInLine(col, rowIndex)
-}
-
-private fun isTreeVisibleInLine(treeLine: List<Int>, index: Int): Boolean {
-    val left = treeLine.subList(0, index)
-    val tree = treeLine[index]
-    val right = treeLine.subList(index + 1, treeLine.size)
-
-    return left.all { it < tree } || right.all { it < tree }
-}
-
-private fun calculateScenicScore(treeGrid: List<List<Int>>, rowIndex: Int, colIndex: Int): Int {
-    if (isTreeAnEdge(treeGrid, rowIndex, colIndex)) return 0
-
-    val row = treeGrid[rowIndex]
-    val col = treeGrid.indices.map { treeGrid[it][colIndex] }
-
-    return calculateScenicScoreInLine(row, colIndex) * calculateScenicScoreInLine(col, rowIndex)
-}
-
-private fun calculateScenicScoreInLine(treeLine: List<Int>, index: Int): Int {
-    val left = treeLine.subList(0, index).reversed()
-    val tree = treeLine[index]
-    val right = treeLine.subList(index + 1, treeLine.size)
-
-    return countTreesUntilBlocked(left, tree) * countTreesUntilBlocked(right, tree)
-}
+private fun part2(treeGrid: List<List<Int>>): Int =
+    treesWithTreesInAllDirections(treeGrid).maxOf { (tree, treesInAllDirections) ->
+        treesInAllDirections
+            .map { countTreesUntilBlocked(it, tree) }
+            .reduce { acc, treesUntilBlocked -> acc * treesUntilBlocked }
+    }
 
 private fun countTreesUntilBlocked(trees: List<Int>, treeHeight: Int): Int {
     for (i in trees.indices) {
@@ -67,5 +31,22 @@ private fun countTreesUntilBlocked(trees: List<Int>, treeHeight: Int): Int {
     return trees.size
 }
 
-private fun isTreeAnEdge(treeGrid: List<List<Int>>, rowIndex: Int, colIndex: Int): Boolean =
-    rowIndex == 0 || rowIndex == treeGrid.size - 1 || colIndex == 0 || colIndex == treeGrid.size - 1
+private fun treesWithTreesInAllDirections(treeGrid: List<List<Int>>): List<Pair<Int, List<List<Int>>>> =
+    treeGrid.flatMapIndexed { rowIndex, row ->
+        row.mapIndexed { colIndex, treeHeight ->
+            treeHeight to treesInAllDirections(
+                treeGrid = treeGrid,
+                rowIndex = rowIndex,
+                colIndex = colIndex
+            )
+        }
+    }
+
+private fun treesInAllDirections(treeGrid: List<List<Int>>, rowIndex: Int, colIndex: Int): List<List<Int>> {
+    val row = treeGrid[rowIndex]
+    val col = treeGrid.indices.map { treeGrid[it][colIndex] }
+    return treesInBothDirections(row, colIndex) + treesInBothDirections(col, rowIndex)
+}
+
+private fun treesInBothDirections(treeLine: List<Int>, index: Int): List<List<Int>> =
+    listOf(treeLine.subList(0, index).reversed(), treeLine.subList(index + 1, treeLine.size))
